c2c8130ea46fcf3eef806d0991a411e447d742d2
mahdi <mahdi.mohseni0000@gmail.com>
2024-07-19 19:56:03 +0330

    serializers.View added

--- cloths/products/views.py
+++ cloths/products/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from .models import Product, Men, Women
+from .serializers import ProductSerializer, MenSerializer, WomenSerializer
 
-# Create your views here.
+from rest_framework import generics
+
+class ProductListCreate(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class MenListCreate(generics.ListCreateAPIView):
+    queryset = Men.objects.all()
+    serializer_class = MenSerializer
+
+class MenDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Men.objects.all()
+    serializer_class = MenSerializer
+
+
+class WomenListCreate(generics.ListCreateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+
+class WomenDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
